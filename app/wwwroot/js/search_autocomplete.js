22d0a469557283f53fddeea17e849808f42981bd
Dean Truong <dtruong8@toromail.csudh.edu>
2020-01-30 13:50:23 -0800

    Autocomplete enhancement
    Enhancing autocomplete.

--- app/wwwroot/js/search_autocomplete.js
+++ app/wwwroot/js/search_autocomplete.js
@@ -1,11 +1,26 @@
-function getListNames() {
-    let name = document.getElementById("contractor_business_name").value;
-
-    if (name.length > 1) {
-        $("contractor_business_name").autocomplete({
-            source: "https://localhost5001/SearchContractor/getAllContractorNames " + name
-        });
+var tags =["Python", "Java","C++", "C"];
+var ContractorNames = [];
+$.ajax({
+    url: 'https://localhost:5001/SearchContractor/getAllContractorNames',
+    method: 'get',
+    dataType: 'json',
+    success: function (data) {
+        JSON.stringify(data);
+        for (var i = 0; i < data.length; i++) {
+            ContractorNames.push(data[i].contractors_business_name);
+        }
+        alert(ContractorNames[0]);
+    },
+    error: function (err) {
+        alert(err);
     }
-}
 
+});
+
+function getListNames() {
+    console.log(ContractorNames);
+    $("#contractor_business_name").autocomplete({
+        source: tags
+    });
+}
 
